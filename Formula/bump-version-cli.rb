b42c053598d2589f942ccccb74e0f49a90dd6381
Raymon Epping <your@email.com>
2025-07-19 16:41:48 +0200

    19/07/2025 - Updated configuration and fixed bugs

---
+++ Formula/bump-version-cli.rb
@@ -0,0 +1,32 @@
+class CommitGhCli < Formula
+  desc "CLI toolkit for visualizing folder structures with markdown reports"
+  homepage "https://github.com/raymonepping/commit_gh_cli"
+  url "https://github.com/raymonepping/homebrew-commit-gh-cli/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "552de6f4f9ef778906ae30e963929233da8a8efdeb5ea762dcf0fa8364e1b279"
+  license "MIT"
+  version "0.0.2"
+
+  depends_on "bash"
+
+  def install
+    bin.install "bin/commit_gh" => "commit_gh"
+    doc.install "README.md"
+  end
+
+  def caveats
+    <<~EOS
+      To get started, run:
+        commit_gh --help
+
+      This CLI helps manage Git commits, tags, and semantic versioning.
+      It uses a .version file for tracking current state.
+
+      Example usage:
+        commit_gh --bump patch --verify
+    EOS
+  end
+
+  test do
+    assert_match "commit_gh", shell_output("#{bin}/commit_gh --help")
+  end
+end
---
+++ sanity_check.md
@@ -0,0 +1,9 @@
+
+---
+## 🕒 Report: 2025-07-19 16:38:55
+
+### 📂 Processed Files
+- `./bin/bump_version.sh`
+
+### ❗ Lint Issues Found
+- `./bin/bump_version.sh`
