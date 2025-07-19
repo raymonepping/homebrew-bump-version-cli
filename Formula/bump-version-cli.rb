4648b423cae59ac5ffc02acb584c72565f658aa0
Raymon Epping <your@email.com>
2025-07-19 22:16:27 +0200

    🔖 bump_version: release v--publish-gh-release

---
+++ Formula/bump-version-cli.rb
@@ -0,0 +1,32 @@
+class BumpVersionCli < Formula
+
+  desc "CLI tool for bumping semantic versions in Bash scripts and updating changelogs"
+  homepage "https://github.com/raymonepping/bump_version_cli"
+  url "https://github.com/raymonepping/homebrew-bump-version-cli/archive/refs/tags/v--publish-gh-release.tar.gz"
+  sha256 "a1ffaf244e32d77acefb3c0a6bf848a33901640c603491c837aaeb81945045e4"
+  license "MIT"
+  version "0.5.11"
+
+  depends_on "bash"
+
+  def install
+    bin.install "bin/bump_version" => "bump_version"
+  end
+
+  def caveats
+    <<~EOS
+      To get started, run:
+        bump_version --help
+
+      This CLI helps manage Git commits, tags, and semantic versioning.
+      It uses a .version file for tracking current state.
+
+      Example usage:
+        bump_version script.sh --minor
+    EOS
+  end
+
+  test do
+    assert_match "bump_version", shell_output("#{bin}/bump_version --help")
+  end
+end
