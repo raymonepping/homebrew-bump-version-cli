a9ffd80c354db96616a3e09786afbcbdb882f8e9
Raymon Epping <your@email.com>
2025-07-19 18:27:58 +0200

    🔖 bump_version: release v0.3.3

--- Formula/bump-version-cli.rb
+++ Formula/bump-version-cli.rb
@@ -2,10 +2,10 @@ class BumpVersionCli < Formula
 
   desc "CLI tool for bumping semantic versions in Bash scripts and updating changelogs"
   homepage "https://github.com/raymonepping/bump_version_cli"
-  url "https://github.com/raymonepping/homebrew-bump-version-cli/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "83cda5b68bbcda84e9656ada5ac9abd214fabb62f5e3a11e3ccdcb66222fe13d"
+  url "https://github.com/raymonepping/homebrew-bump-version-cli/archive/refs/tags/v0.3.3.tar.gz"
+  sha256 "b1406ee82428d55d144929b2970ad2f2db45baacf5c49a4e29876c9d4654666b"
   license "MIT"
-  version "0.3.2"
+  version "0.3.3"
 
   depends_on "bash"
 
